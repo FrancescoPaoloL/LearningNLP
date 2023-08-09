2f21f03108fffdb57f522ad41e0ec5522b5dc75f
fpl <francescopaololezza@gmail.com>
2023-08-09 22:01:21 +0200

    Minor modified to dependency main file

--- DependencyGrammar/main.py
+++ DependencyGrammar/main.py
@@ -1,5 +1,5 @@
 from dependency_tree import draw_dependency_tree
 
 if __name__ == "__main__":
-    input_sentence = "The brown fox is quick and he is jumping over the lazy dog"
+    input_sentence = input()
     draw_dependency_tree(input_sentence)
