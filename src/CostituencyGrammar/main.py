b987b5ebde259a68105fb9c6d4abc2c82867909e
fpl <francescopaololezza@gmail.com>
2023-09-20 21:51:44 +0200

    Minor changes done in Costituency Grammar project

--- src/CostituencyGrammar/main.py
+++ src/CostituencyGrammar/main.py
@@ -2,9 +2,9 @@
 
 import stanza
 
-nlp = stanza.Pipeline(lang='en', processors='tokenize,pos,constituency')
-sentence = input()
-doc = nlp(sentence)
-for sentence in doc.sentences:
-    print(sentence.constituency)
-
+if __name__ == "__main__":
+    nlp = stanza.Pipeline(lang='en', processors='tokenize,pos,constituency')
+    sentence = input()
+    doc = nlp(sentence)
+    for sentence in doc.sentences:
+        print(sentence.constituency)
