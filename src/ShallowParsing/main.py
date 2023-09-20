b26c8eaeae8e55475c1222a413cab19400a1a52e
fpl <francescopaololezza@gmail.com>
2023-09-20 21:31:42 +0200

    Minor changes done in Shallow Parsing project

--- src/ShallowParsing/main.py
+++ src/ShallowParsing/main.py
@@ -1,7 +1,10 @@
 from nltk_utils import shallow_parse, print_tree
 
 if __name__ == "__main__":
-    sentence = input()
+    print("Please insert a phrase. I suggest a default example but you can write anything you want.")
+    default_input = "The brown fox is quick and he is jumping over the lazy dog"
+    sentence = input(f"Example: {default_input}. ")
+    sentence = sentence.strip() or default_input
 
     parsed_tree = shallow_parse(sentence, "src/ShallowParsing/grammar_config.yaml")
     print_tree(parsed_tree)
