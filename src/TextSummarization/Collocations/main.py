3beea3d59d5bb9613b9dabb95e956a59f31f8fc5
fpl <francescopaololezza@gmail.com>
2023-11-24 05:51:44 +0100

    Elimitated redundant comments

--- src/TextSummarization/Collocations/main.py
+++ src/TextSummarization/Collocations/main.py
@@ -1,6 +1,7 @@
 from nltk.corpus import gutenberg
 from operator import itemgetter
 from nlp_utils import preprocess_text, extract_collocations
+from nltk import FreqDist
 
 def download_nltk_resources():
     import nltk
@@ -8,16 +9,10 @@ def download_nltk_resources():
     nltk.download("stopwords")
 
 def main():
-    # Download NLTK resources
     download_nltk_resources()
-
-    # Load "The Great Gatsby" text from Gutenberg
     gatsby = gutenberg.raw("burgess-busterbrown.txt")
-
-    # Tokenize and normalize the text
     filtered_tokens = preprocess_text(gatsby)
 
-    # Extract bigrams
     collocations = extract_collocations(filtered_tokens)
 
     # Display collocations
@@ -25,11 +20,9 @@ def main():
         print(' '.join(collocation))
 
     # Sort collocations by frequency
-    from nltk import FreqDist
     fdist = FreqDist(collocations)
     sorted_collocations = sorted(fdist.items(), key=itemgetter(1), reverse=True)
 
-    # Display sorted collocations
     print("\nTop Collocations:")
     for collocation, frequency in sorted_collocations:
         print(f"{collocation}: {frequency}")
