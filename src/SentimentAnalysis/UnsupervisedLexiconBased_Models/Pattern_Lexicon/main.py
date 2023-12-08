fe64185c13b8994ceb1043766516811c91bee1e4
fpl <francescopaololezza@gmail.com>
2023-12-08 17:14:12 +0100

    Implemented Patter Lexicon logic example

---
+++ src/SentimentAnalysis/UnsupervisedLexiconBased_Models/Pattern_Lexicon/main.py
@@ -0,0 +1,12 @@
+from pattern.en import sentiment
+
+def analyze_sentiment(text):
+    polarity, subjectivity = sentiment(text)
+    return polarity, subjectivity
+
+hardcoded_text = "I love using Python for programming. It's such a powerful and versatile language!"
+
+print(f"Sentiment Analysis Results:")
+print(f"Polarity: {polarity}")
+print(f"Subjectivity: {subjectivity}")
+
